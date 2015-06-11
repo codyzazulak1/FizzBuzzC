6a8e09646b33ed472619438f36d73a4b38b58837
codyzazulak1 <codyzazulak1@gmail.com>
2015-06-11 03:28:39 +0000

    Completed

--- C-Starter-Project/main.c
+++ C-Starter-Project/main.c
@@ -9,7 +9,20 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int x;
+    
+    for (x = 1; x <= 100; x++) {
+        if ((x % 5 == 0) && (x % 3 == 0)) {
+            printf("FizzBuzz\n");
+        } else if (x % 5 == 0) {
+            printf("Buzz\n");
+        } else if (x % 3 == 0) {
+            printf("Fizz\n");
+        } else {
+            printf("%d\n", x);
+        }
+    }
+    
     return 0;
 }
